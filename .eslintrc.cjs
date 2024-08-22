2f48744ab8cd188524cfd39fb274e797e2ee2bcd
David Bosschaert <bosschae@adobe.com>
2024-08-22 11:23:34 -0400

    Take the latest parser code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,8 +12,51 @@
 
 module.exports = {
   root: true,
-  extends: '@adobe/helix',
-  rules: {
-    'no-await-in-loop': 0,
+  extends: 'airbnb-base',
+  env: { browser: true, mocha: true },
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    requireConfigFile: false,
   },
+  rules: {
+    'import/no-unresolved': 'off',
+    'no-param-reassign': [2, { props: false }],
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['error', { js: 'always' }],
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, minProperties: 6 },
+      ObjectPattern: { multiline: true, minProperties: 6 },
+      ImportDeclaration: { multiline: true, minProperties: 6 },
+      ExportDeclaration: { multiline: true, minProperties: 6 },
+    }],
+    'no-await-in-loop': 0,
+    'class-methods-use-this': 0,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-unused-expressions': 0,
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['test/**/*.js'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
 };
